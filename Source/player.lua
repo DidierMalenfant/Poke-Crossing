09546b17967e908ae6c41f3164c878b5ca6261e5
Didier Malenfant <coding@malenfant.net>
2022-07-05 20:07:52 +0200

    Improved player dynamics and added collision support

--- Source/player.lua
+++ Source/player.lua
@@ -9,67 +9,86 @@ import "CoreLibs/sprites"
 local gfx <const> = playdate.graphics
 local snd <const> = playdate.sound
 
+local gravity <const> = 1.2
+local jump_force <const> = 15.0
+local max_fall_speed <const> = 3.0
+local move_force_on_ground <const> = 5.0
+local move_force_in_air <const> = 1.0
+local max_move_force <const> = 5.0
+local lateral_friction <const> = 0.4
+
 class('Player').extends(gfx.sprite)
 
-function Player:init(x, y, imagePath)
+function Player:init(x, y, image_path)
     Player.super.init(self) -- this is critical
 
-    self.image = gfx.image.new(imagePath)
-    assert(self.image)
+    self.image = gfx.image.new(image_path)
+    assert(self.image, 'Error creating a new image.')
 
     self:setImage(self.image)
-    self:setCollideRect(0, 0, self.width, self.height)
+    self:setCollideRect(10, 0, self.width - 20, self.height)
+
+    self:setZIndex(10)
+    --self:setCenter(0, 0)	-- set center point to center bottom
 
     self.dx = 0.0
     self.dy = 0.0
-    self.last_dy = 0.0
-
-    self.jump_time = 0.0
 
     self:moveTo(x, y)
     self:add()
 
     self.jump_sound = snd.sampleplayer.new('Sounds/jump')
-    assert(self.jump_sound)
+    assert(self.jump_sound, 'Error loading jump sound.')
 end
 
 function Player:update()
-    if self.jump_time ~= 0.0 then
-        self.dy = playdate.easingFunctions.inOutSine(self.jump_time, 0.0, -60.0, 4.0)
-        self.jump_time += 0.3
-
-        if self.jump_time > 8.0 then
-            self.dy = 0.0
-            self.jump_time = 0.0
-        end
+    self.dy += gravity
+    if self.dy > 0.0 then
+        self.dy = math.max(self.dy, max_fall_speed)
     end
 
-    self:moveWithCollisions(self.x + self.dx, self.y + self.dy - self.last_dy)
-    self.last_dy =  self.dy
+    local wanted_x = self.x + self.dx
+    local wanted_y = self.y + self.dy
 
-    if self.dx > 0.0 then
-        self.dx -= math.min(0.08, self.dx)
+    local actual_x, actual_y, _, _ = self:moveWithCollisions(wanted_x, wanted_y)
+
+    if actual_x ~= wanted_x then
+        self.dx = 0.0
+    elseif self.dx > 0.0 then
+        self.dx -= math.min(lateral_friction, self.dx)
     elseif self.dx < 0.0 then
-        self.dx -= math.max(-0.08, self.dx)
+        self.dx -= math.max(-lateral_friction, self.dx)
+    end
+
+    if actual_y ~= wanted_y then
+        self.dy = 0.0
     end
 end
 
 function Player:goLeft()
-    if self.dx > -2.0 then
-        self.dx -= 0.5
+    if self:isJumping() == true then
+        self.dx -= move_force_in_air
+    else
+        self.dx -= move_force_on_ground
     end
+
+    self.dx = math.max(-max_move_force, self.dx)
 end
 
 function Player:goRight()
-    if self.dx < 2.0 then
-        self.dx += 0.5
+    if self:isJumping() == true then
+        self.dx += move_force_in_air
+    else
+        self.dx += move_force_on_ground
     end
+
+    self.dx = math.min(max_move_force, self.dx)
 end
 
 function Player:jump()
-    if self.jump_time == 0 then
+    if self:isJumping() ~= true then
         self.jump_sound:play()
-        self.jump_time += 0.1
+        self.dy = -jump_force
     end
 end
 
@@ -78,16 +97,10 @@ function Player:turn(angle)
 end
 
 function Player:isJumping()
-    return self.jump_time ~= 0.0
+    return self.dy ~= 0.0
 end
 
-function Player:collisionResponse(other) -- luacheck: ignore self
-    local tag = other:getTag()
-
-    if tag == 0 then
-        self.dx = 0.0
-    end
-
+function Player:collisionResponse(other) -- luacheck: ignore self other
     return gfx.sprite.kCollisionTypeSlide
 end
 
